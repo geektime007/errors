015b113c03c61d55de286a79442fa8c5de6f793b
YZT <yunzhongtan@outlook.com>
2021-05-10 21:24:11 +0800

    remove error type

--- errors.go
+++ errors.go
@@ -7,10 +7,10 @@ package errors
 
 var (
 	// 通用错误
-	Nil             *Error = nil
-	Unknown                = NewError(ErrorCodeUnknown, 500, "未知错误")
-	BadFormat              = NewError(ErrorCodeBadFormat, 400, "格式错误")
-	RunCommandError        = NewError(ErrorRunCommandError, 500, "执行命令异常")
-	Timeout                = NewError(ErrorCodeTimeout, 408, "超时")
-	Unimplemented          = NewError(ErrorCodeUnimplemented, 404, "未实现!")
+	Nil *Error = nil
+	//Unknown                = NewError(ErrorCodeUnknown, 500, "未知错误")
+	//BadFormat              = NewError(ErrorCodeBadFormat, 400, "格式错误")
+	//RunCommandError        = NewError(ErrorRunCommandError, 500, "执行命令异常")
+	//Timeout                = NewError(ErrorCodeTimeout, 408, "超时")
+	//Unimplemented          = NewError(ErrorCodeUnimplemented, 404, "未实现!")
 )
